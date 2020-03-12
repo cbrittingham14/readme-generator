562f64a5eefe82044ab04fcb52bb936cdd29be32
chizz <cbrittingham@gmail.com>
2020-03-12 17:17:29 -0500

    fleshed out question object for inquirer. added .DS_Store to .gitignore file

--- index.js
+++ index.js
@@ -2,12 +2,73 @@ const inquirer = require('inquirer');
 const axios = require('axios');
 const fs = require('fs');
 
-inquirer
-    .prompt({
+let questions = [
+    {
         message:"What is github user name?",
         type: 'input',
         name: "username",
-    })
+    },
+    {
+        message: 'what is the title of your project?',
+        type:'input',
+        name:'title'
+    },
+    {
+        message: 'what is the description of your project?',
+        type: 'input',
+        name:'description'
+    },
+    {
+        message: 'What license will you use?',
+        type: 'list',
+        choices:[
+            {
+                name: "Apache 2.0",
+                value: 'apache'
+            }, 
+            {
+                name: "GNU General Public License 3.0",
+                value: 'gnu'}, 
+            {
+                name: "MIT", 
+                value: 'mit'
+            },
+             {
+                 name: "None", 
+                 value: 'none'
+            }
+        ],
+        name:'license',
+    },
+    {
+        message:'what is the method of installation?',
+        type:'input',
+        name:'installation'
+    },
+    {
+        message: 'what is the intended use of your application?',
+        type: 'input',
+        name: 'use'
+    },
+    {
+        message:'how do people contribute?',
+        type: 'input',
+        name:'contribute'
+    },
+    {
+        message:'how do you test this application?',
+        type: 'input',
+        name:'test'
+    },
+    {
+        message:'how will you accept questions about this application?',
+        type:'input',
+        name:'questions'
+    }
+
+]
+inquirer
+    .prompt(questions)
     .then(function({ username }) {
         const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
         axios

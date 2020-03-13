0f43ba4262b2fe1c0bf7c156fcad5a2f8f70bfb3
chizz <cbrittingham@gmail.com>
2020-03-13 09:21:51 -0500

    switched readme formatting to html after having trouble with .md. added logic to select the correct badge image url

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const mitSrc = "https://camo.githubusercontent.com/2b5c48821f22738887c98a07f9585
 const gnuSrc = "https://camo.githubusercontent.com/581ed12f23a2dc1a36eb7c10dc01664456d9df71/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c6963656e73652d47504c25323076332d79656c6c6f772e737667"
 const apacheSrc = "https://camo.githubusercontent.com/6e97fc42cf3a7104864fabeae2596615886e6736/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6c6963656e73652d4147504c2d626c75652e737667"
 let markDown;
-
-let questions = [
+let badgeURL;
+const questions = [
     {
         message:"What is github user name?",
         type: 'input',
@@ -71,7 +71,10 @@ let questions = [
 inquirer
     .prompt(questions)
     .then(function(inputs) {
+        let { license } = inputs;
+        getBadgeURL(license);
         markDown = makeMarkdown(inputs);
+        console.log(inputs);
         const queryUrl = `https://api.github.com/users/${inputs.username}/events/public`;
         axios
           .get(queryUrl)
@@ -80,7 +83,7 @@ inquirer
               userEmail = data[0].payload.commits[0].author.email;
               console.log(gitImageURL);
               console.log(userEmail);
-              fs.writeFile("README.md", markDown, function(err){
+              fs.writeFile("readme.html", markDown, function(err){
                 if(err){
                     console.log(err);
                 }
@@ -92,12 +95,37 @@ inquirer
           });
     });      
 
-function makeMarkdown(data){
-    var markdown = `#${data.title}
-    ##Description
-    ${data.description}
-    ##License
-    [Apache](${apacheSrc})
-    `;
-    return markdown;
-}
+    function getBadgeURL(lic){
+        if(lic === 'apache'){
+            badgeURL = apacheSrc;
+        } else if (lic === 'mit'){
+            badgeURL = mitSrc;
+        } else{
+            badgeURL = gnuSrc;
+        }
+    }
+    function makeMarkdown(data){
+        var markdown = `<!DOCTYPE=html>
+        <title>${data.title}</title>
+        <h1>#${data.title}<h1>
+        <h2>##Description</h2>
+        <div>${data.description}</div>
+        <h2>##License</h2>
+        <img src="${badgeURL}" alt="${data.license} license" />
+        `;
+        return markdown;
+    }    
+// function makeMarkdown(data){
+//     var markdown = `# ${data.title}
+
+//     ## Description
+
+//     ${data.description}
+
+//     ## License
+
+//     ![Apache]("${apacheSrc}")
+
+//     `;
+//     return markdown;
+// }
